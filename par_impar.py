fec69a787502138258e6b6f01e03a3433fab3773
AndersonFonseca1704 <anderson2214103@correo.uis.edu.co>
2022-04-25 09:33:29 -0500

    par_impar

---
+++ par_impar.py
@@ -0,0 +1,10 @@
+num = input("intoduce un numero: ")
+num =int(num)
+
+
+if num == 0:
+    print("este numero es par.")
+elif num%2 ==0:
+    print("este numero es par")
+else:
+    print("este numero es impar")
